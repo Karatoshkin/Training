6459d01f69847d955851a1cba3850fbf66b2dada
Karatoshkin <andreyvmiha20926@gmail.com>
2023-01-06 16:40:46 +0400

    Первые функции

--- Training/main.py
+++ Training/main.py
@@ -1,1 +1,29 @@
-print("hi")
+month= {
+  1:"Январь",
+  2:"Февраль",
+  3:"Март",
+  4:"Апрель",
+  5:"Май",
+  6:"Июнь",
+  7:"Июль",
+  8:"Август",
+  9:"Сентябрь",
+  10:"Октябрь",
+  11:"Ноябрь",
+  12:"Декабрь"
+}
+
+print ("Укажите месяц")
+def seasons(number_of_month):
+  if number_of_month in range (3, 5):
+    print (f"Вы указали {month[number_of_month]} - Весна")
+  elif number_of_month in range (6, 8):
+    print (f"Вы указали {month[number_of_month]} - Лето")
+  elif number_of_month in range (9, 11):
+    print (f"Вы указали {month[number_of_month]} - Осень")
+  elif number_of_month in (12, 1, 2):
+    print (f"Вы указали {month[number_of_month]} - Зима")
+  else:
+    print (f"Ты чё ебанутый? Тебе сказали номер месяца, у нас что, есть {number_of_month}-ый месяц??")
+
+seasons(2)
